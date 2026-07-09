c9427a9293eb07d95ac93de949484baf303b698d
krishna <krishna.42971@gmail.com>
2026-07-09 19:56:09 +0530

    add db

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,88 @@
+import { google } from "@ai-sdk/google";
+import z from "zod";
+import {
+    streamText,
+    UIMessage,
+    convertToModelMessages,
+    createUIMessageStreamResponse,
+    toUIMessageStream,
+    tool,
+    stepCountIs
+} from 'ai';
+import { db, turso } from "@/src/db/db";
+
+//Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+export async function POST(req: Request) {
+    const { messages }: { messages: UIMessage[] } = await req.json();
+
+    const SYSTEM_PROMPT = `You are an expert SQL assistant that helps users to query the database using natural language.
+    You have access to following tools:
+
+    current date and time: ${new Date().toLocaleString('sv-SE')}
+    1. Schema tool - call this tool to get the database schema which will help you to write SQL query.
+    2. db tool - call this tool to query the database.
+    
+    Rules:
+    -Generate the only SELECT queries(no INSERT, UPDATE, DELETE, DROP)
+    -Always use the schema provided by the schema tool
+    -Pass in valid SQL syntax in db tool.
+    -IMPORTANT: To query database call db tool, Don't return just SQL query.
+    
+    Always respond in a helpful, conversational tone whole being technically accurate.`;
+
+    const result = streamText({
+        model: google("gemini-2.5-flash"),
+        messages: await convertToModelMessages(messages),
+        system: SYSTEM_PROMPT,
+        stopWhen: stepCountIs(5),
+        tools: {
+            schema: tool({
+                description: 'Call this tool to get database schema information.',
+                inputSchema: z.object({}),
+                execute: async () => {
+                    return `CREATE TABLE products (
+	id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	name text NOT NULL,
+	ategory text NOT NULL,
+	price real NOT NULL,
+	stock integer DEFAULT 0 NOT NULL,
+	created_at text DEFAULT CURRENT_TIMESTAMP
+)
+
+CREATE TABLE sales (
+	id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	product_id integer NOT NULL,
+	quantity integer NOT NULL,
+	total_amount real NOT NULL,
+	sale_date text DEFAULT CURRENT_TIMESTAMP,
+	customer_name text NOT NULL,
+	region text NOT NULL,
+	FOREIGN KEY (product_id) REFERENCES products(id) ON UPDATE no action ON DELETE no action
+)`;
+                },
+            }),
+            db: tool({
+                description: 'Call this tool to query a database',
+                inputSchema: z.object({
+                    query: z.string().describe('The SQL query to be ran'),
+                }),
+                execute: async ({ query }) => {
+                    // console.log('Query', query);
+                    // // Important: make sure you sanitize / validate (somehow) check the query
+                    // // string search [delete, update] -> Guadrails
+                    // return await turso.execute(query);
+                    console.log(query);
+
+                    const result = await turso.execute(query);
+
+                    return result.rows;
+                },
+            }),
+        },
+    });
+
+    return createUIMessageStreamResponse({
+        stream: toUIMessageStream({ stream: result.stream }),
+    });
+}
